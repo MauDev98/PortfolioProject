2d39e846f904664383dc5627ca87097287828f21
maudev <mauriciodavidfloresvaldivia1@gmail.com>
2021-11-22 11:22:35 -0500

    portfolio project

---
+++ resources/js/scripts.js
@@ -0,0 +1,9 @@
+// NAVIGATION BAR SCRIPTS START
+navigationBar = document.getElementById('navigationBar');
+
+function navbarToggle() {
+  navigationBar.classList.remove('nav-container');
+  navigationBar.classList.add('nav-container-responsive')
+}
+
+
