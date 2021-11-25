4f4ca0efcfe0c3d1609037c5063bc77be6e9d9c2
mauriciodavidfloresvaldivia1@gmail.com <mauriciodavidfloresvaldivia1@gmail.com>
2021-11-25 14:06:35 -0500

    navbar work in progress

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,9 +1,16 @@
 // NAVIGATION BAR SCRIPTS START
-navigationBar = document.getElementById('navigationBar');
+const navigationBar = document.getElementById('navigationBar');
+const navBtnClose = document.getElementById('navBtnClose')
+
 
 function navbarToggle() {
   navigationBar.classList.remove('nav-container');
-  navigationBar.classList.add('nav-container-responsive')
+  navigationBar.classList.add('nav-container-responsive', 'is-visible')
+  navBtnClose.classList.remove('not-visible')
 }
 
+function navbarClose() {
+  navigationBar.classList.remove('is-visible')
+  navBtnClose.classList.add('not-visible')
+}
 
