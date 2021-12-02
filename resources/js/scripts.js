bf15cc99fdd9959c70869e23f03e8e5c2f501561
MauDev98 <mauriciodavidfloresvaldivia1@gmail.com>
2021-12-02 07:50:41 -0500

    Upgraded navbar and welcome background

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,16 +1,34 @@
 // NAVIGATION BAR SCRIPTS START
-const navigationBar = document.getElementById('navigationBar');
-const navBtnClose = document.getElementById('navBtnClose')
+// BUTTONS FUNCTIONS
 
+const navigationBar = document.getElementById("navigationBar");
+const navBtnClose = document.getElementById("navBtnClose");
+const responsiveBtn = document.getElementById("responsive-button");
 
 function navbarToggle() {
-  navigationBar.classList.remove('nav-container');
-  navigationBar.classList.add('nav-container-responsive', 'is-visible')
-  navBtnClose.classList.remove('not-visible')
+  navigationBar.classList.add("is-visible");
+  navBtnClose.classList.remove("not-visible");
+  responsiveBtn.classList.add("not-visible");
 }
 
 function navbarClose() {
-  navigationBar.classList.remove('is-visible')
-  navBtnClose.classList.add('not-visible')
+  navigationBar.classList.remove("is-visible");
+  navBtnClose.classList.add("not-visible");
+  responsiveBtn.classList.remove("not-visible");
 }
+
+//STICKY NAVBAR CODE
+
+const wholeNavbar = document.getElementById("complete-navbar");
+let navbarPosition = wholeNavbar.offsetTop;
+
+function navbarOnScroll() {
+  if (window.scrollY > navbarPosition + 100) {
+    wholeNavbar.classList.add("navigation-bar-scroll");
+  } else {
+    wholeNavbar.classList.remove("navigation-bar-scroll");
+  }
+}
+
+window.addEventListener("scroll", navbarOnScroll);
 
