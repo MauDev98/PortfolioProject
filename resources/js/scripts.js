d1375f4607466d1881016ae80308dfbab4b87597
MauDev98 <mauriciodavidfloresvaldivia1@gmail.com>
2021-12-03 18:33:49 -0500

    updated portfolio project 3/12

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -32,3 +32,23 @@ function navbarOnScroll() {
 
 window.addEventListener("scroll", navbarOnScroll);
 
+//SMOOTH SCROLL EFFECT
+const links = document.querySelectorAll(".nav-link");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+}
+
+
+
