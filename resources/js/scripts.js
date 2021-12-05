ed89d73f6ed3afebd9a5269b2f800d58fe6df061
MauDev98 <mauriciodavidfloresvaldivia1@gmail.com>
2021-12-05 10:22:55 -0500

    "formatted with js-css-html beutify"

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -6,16 +6,18 @@ const navBtnClose = document.getElementById("navBtnClose");
 const responsiveBtn = document.getElementById("responsive-button");
 
 function navbarToggle() {
-  navigationBar.classList.add("is-visible");
-  navBtnClose.classList.remove("not-visible");
-  responsiveBtn.classList.add("not-visible");
+    navigationBar.classList.add("is-visible");
+    navBtnClose.classList.remove("not-visible");
+    responsiveBtn.classList.add("not-visible");
 }
 
+
 function navbarClose() {
-  navigationBar.classList.remove("is-visible");
-  navBtnClose.classList.add("not-visible");
-  responsiveBtn.classList.remove("not-visible");
+    navigationBar.classList.remove("is-visible");
+    navBtnClose.classList.add("not-visible");
+    responsiveBtn.classList.remove("not-visible");
 }
+
 
 //STICKY NAVBAR CODE
 
@@ -23,11 +25,11 @@ const wholeNavbar = document.getElementById("complete-navbar");
 let navbarPosition = wholeNavbar.offsetTop;
 
 function navbarOnScroll() {
-  if (window.scrollY > navbarPosition + 100) {
-    wholeNavbar.classList.add("navigation-bar-scroll");
-  } else {
-    wholeNavbar.classList.remove("navigation-bar-scroll");
-  }
+    if (window.scrollY > navbarPosition + 100) {
+        wholeNavbar.classList.add("navigation-bar-scroll");
+    } else {
+        wholeNavbar.classList.remove("navigation-bar-scroll");
+    }
 }
 
 window.addEventListener("scroll", navbarOnScroll);
@@ -36,16 +38,16 @@ window.addEventListener("scroll", navbarOnScroll);
 const links = document.querySelectorAll(".nav-link");
 
 for (const link of links) {
-  link.addEventListener("click", clickHandler);
+    link.addEventListener("click", clickHandler);
 }
 
 function clickHandler(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  const offsetTop = document.querySelector(href).offsetTop;
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
 
-  scroll({
-    top: offsetTop,
-    behavior: "smooth",
-  });
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    });
 }
