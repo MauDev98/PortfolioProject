743c15c9954a0d4d1172cfa943c65deac99f8ddd
MauDev98 <mauriciodavidfloresvaldivia1@gmail.com>
2021-12-15 13:47:57 -0500

    No real advance haha

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -11,7 +11,6 @@ function navbarToggle() {
     responsiveBtn.classList.add("not-visible");
 }
 
-
 function navbarClose() {
     navigationBar.classList.remove("is-visible");
     navBtnClose.classList.add("not-visible");
@@ -62,36 +61,41 @@ function hasClass(ele, className) {
     }
 }
 
-function togglePopUp(button, popUp, event) {
+// This function will take a button, a hidden element to be shown and the section where those elements are 
+// hidden element with the not-visible class is needed, it's counterpart is-visible must be defines as well
+function togglePopUp(button, popUp, section) {
     const buttonEle = document.getElementById(button);
     const popUpEle = document.getElementById(popUp);
-    buttonEle.addEventListener(event, function(e) {
-        const makeVisible = "is-visible"
-        if (hasClass(popUpEle, makeVisible) == true) {
-            popUpEle.classList.remove(makeVisible)
-            e.stopPropagation();
-        } else {
-            popUpEle.classList.add(makeVisible)
-            e.stopPropagation();
-        }
-    })
-    buttonEle.addEventListener(event, function(){
-        const makeVisible = "is-visible"
-        const technologiesSection = document.getElementById("technologies-sec");
-        const notVisibleElements = technologiesSection.querySelectorAll(".not-visible")
+    const makeVisible = "is-visible"
+    const currentSection = document.getElementById(section);
+    const notVisibleElements = currentSection.querySelectorAll(".not-visible")
+    buttonEle.addEventListener("click", function(e) {
+        //here is where the magic happens:
+        //if another hidden element is shown in the section, making this element visible will hide it
         for (i = 0; i < notVisibleElements.length; i++) {
             if (hasClass(notVisibleElements[i], makeVisible) && notVisibleElements[i] != popUpEle){
                 notVisibleElements[i].classList.remove(makeVisible)
             }
         }
+        //if the button is pressed again while the popUp is visible, the click will toggle it
+        if (hasClass(popUpEle, makeVisible) == true) {
+            popUpEle.classList.remove(makeVisible)
+            e.stopPropagation();
+        //if the button is pressed the hidden element is shown 
+        } else {
+            popUpEle.classList.add(makeVisible)
+            e.stopPropagation();
+        }
     })
-    window.addEventListener(event, function(e) {
+    window.addEventListener("click", function(e) {
+        //additionally, if any hidden element is visible, clicking any other part in the page will hide it 
         const makeVisible = "is-visible"
         const eleAndChildren = popUpEle.children;
         for (i in eleAndChildren) {
             let currentTarget = e.target
             if (popUpEle.contains(currentTarget)) {
                 break
+                //but clicking inside the popUp will not hide it
             } else {
                 if (hasClass(popUpEle, makeVisible)) {
                     popUpEle.classList.remove(makeVisible)
@@ -101,17 +105,9 @@ function togglePopUp(button, popUp, event) {
     })
 }
 
-togglePopUp("css-framework-button", "css-framework-div", "click")
-togglePopUp("js-framework-button", "js-framework-div", "click")
-togglePopUp("learn-more-linux", "linux-bubble", "click")
-togglePopUp("learn-more-php", "php-bubble", "click")
-togglePopUp("workflow-button", "workflow-bubble", "click")
-
-
-
-
-
-
-
-
-//Fix this by removing not visible class to the p tag, idiot
+//adding toggling to all technologies section buttons
+togglePopUp("css-framework-button", "css-framework-div", "technologies-sec")
+togglePopUp("js-framework-button", "js-framework-div", "technologies-sec")
+togglePopUp("learn-more-linux", "linux-bubble", "technologies-sec")
+togglePopUp("learn-more-php", "php-bubble", "technologies-sec")
+togglePopUp("workflow-button", "workflow-bubble", "technologies-sec")
